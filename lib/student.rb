66cfd4ba86e3670e65b17e46a0bc8e2d5a4c6c5f
Sophie Gu <sophieqgu@gmail.com>
2019-06-13 20:03:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,12 +38,5 @@ class Student
     student.save 
     student 
   end 
-end 
-    
-      
-      
-    
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+
 end
