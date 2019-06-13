41fb358587e609bdf6fc0f97c1f5bd97fbb7dfef
Sophie Gu <sophieqgu@gmail.com>
2019-06-13 20:06:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,11 @@
 class Student
   attr_accessor :name, :grade
-  attr_accessor :id 
+  attr_reader :id 
   
   def initialize(name, grade, id = nil)
     @name = name 
     @grade = grade 
+    @id = id 
   end 
   
   def self.create_table 
